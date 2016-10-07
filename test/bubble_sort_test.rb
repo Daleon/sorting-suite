1d803e3d3e90b1739603771c3f02ad8c050bb87e
Daleon <Rugfia@yahoo.com>
2016-10-06 21:57:56 -0600

    Update bubble_sort_test.rb

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 require '../lib/bubble_sort'
 
 class BubbleSortTest < Minitest::Test
-  def test_it_can_sort
+  def test_it_can_bubble_sort
     sorter = BubbleSort.new
     assert_equal ["a", "b", "c", "d"],
      sorter.sort(["d", "b", "a", "c"])
