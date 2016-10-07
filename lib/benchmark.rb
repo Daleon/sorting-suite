ca447031c03329bce792fcd32d8ed75b4ae61c44
Daleon <Rugfia@yahoo.com>
2016-10-06 22:38:59 -0600

    Update benchmark.rb

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -7,16 +7,16 @@ beginning_time = Time.now
 sorter = BubbleSort.new
  sorter.sort(["d", "b", "a", "c"])
 end_time = Time.now
-puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds for bubble_sort.rb"
+puts "Time elapsed #{(end_time - beginning_time)*1000000} microseconds for bubble_sort.rb"
 
 beginning_time_2 = Time.now
 insertion_sorter = InsertSort.new
   insertion_sorter.sort(["d", "b", "a", "c"])
 end_time_2 = Time.now
-puts "Time elapsed #{(end_time_2 - beginning_time_2)*1000} milliseconds for insert_sort.rb"
+puts "Time elapsed #{(end_time_2 - beginning_time_2)*1000000} microseconds for insert_sort.rb"
 
 beginning_time_3 = Time.now
 merge_sorter = MergeSort.new
   merge_sorter.merge_sort(["d", "b", "a", "c"])
 end_time_3 = Time.now
-puts "Time elapsed #{(end_time_3 - beginning_time_3)*1000} milliseconds for merge_sort.rb"
+puts "Time elapsed #{(end_time_3 - beginning_time_3)*1000000} microseconds for merge_sort.rb"
